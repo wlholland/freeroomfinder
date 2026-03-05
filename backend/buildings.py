89cececbabc057e1814dc708a56d94a407c58856
wlholland <will@hollandfam.com>
2026-03-04 21:54:55 -0700

    fixed building list to actual names

--- backend/buildings.py
+++ backend/buildings.py
@@ -2,74 +2,74 @@ BUILDING_NAMES: dict[str, str] = {
     "ARTS": "Arts Building",
     "B66": "B-66",
     "B67": "B-67",
-    "BNSN": "Benson Building",
-    "BRLW": "Barlow Building",
-    "BRMB": "Brimhall Building",
-    "CANC": "Cancer Research Center",
-    "CB": "Crabtree Building",
+    "BNSN": "Ezra Taft Benson Building",
+    "BRLW": "Barlow Center",
+    "BRMB": "George H. Brimhall Building",
+    "CANC": "Cannon Center",                      # was: "Cancer Research Center" (wrong)
+    "CB": "W.W. Clyde Building",                  # was: "Crabtree Building" (wrong)
     "CONF": "Conference Center",
-    "CTB": "Clyde Building",
+    "CTB": "Crabtree Technology Building",        # was: "Clyde Building" (wrong)
     "DIST": "Distance Education",
     "EB": "Engineering Building",
-    "ELLB": "Ellsworth Building",
-    "ESC": "Earth Science Building",
-    "FSSS": "Family Sciences Building",
+    "ELLB": "Leo Ellsworth Building",
+    "ESC": "Eyring Science Center",               # was: "Earth Science Building" (wrong)
+    "FSSS": "Film Student Support Services",      # was: "Family Sciences Building" (wrong)
     "GLFG": "Golf Course",
     "HAWF": "Harman Building",
     "HBLL": "Harold B. Lee Library",
-    "HC": "Heritage Halls Commons",
-    "HCEB": "Hinckley Center",
-    "HGB": "Harris Fine Arts Center",
+    "HC": "Hinckley Alumni & Visitors Center",    # was: "Heritage Halls Commons" (wrong)
+    "HCEB": "Harman Building",                    # was: "Hinckley Center" (wrong)
+    "HGB": "Heber J. Grant Building",             # was: "Harris Fine Arts Center" (wrong)
     "HLRA": "Helaman Halls",
     "HOSP": "Hospital",
-    "HRCB": "Harold R. Clark Building",
+    "HRCB": "Herald R. Clark Building",           # was: "Harold R. Clark Building" (wrong first name)
     "HRCN": "HRCN",
     "IPF": "Indoor Practice Facility",
     "JFSB": "Joseph F. Smith Building",
-    "JKB": "Jesse Knight Building",
-    "JRCB": "John R. Christiansen Building",
+    "JKB": "Jesse Knight Humanities Building",
+    "JRCB": "J. Reuben Clark Building",           # was: "John R. Christiansen Building" (wrong)
     "JSB": "Joseph Smith Building",
-    "KMBL": "Kimball Tower",
-    "LNDC": "LNDC",
+    "KMBL": "Spencer W. Kimball Tower",
+    "LNDC": "London Center",
     "LSB": "Life Sciences Building",
-    "LSGH": "Life Sciences Greenhouse",
+    "LSGH": "Life Sciences Greenhouses",
     "LVES": "LaVell Edwards Stadium",
-    "MARB": "Maeser Building",
-    "MB": "Museum of Art Basement",
-    "MC": "Monte L. Bean Museum",
-    "MCA": "MCA",
-    "MCDB": "MCDB",
-    "MCKB": "McKay Building",
-    "MLBM": "MLBM",
+    "MARB": "Thomas L. Martin Building",          # was: unlabeled (now confirmed)
+    "MB": "Music Building",                       # was: "Museum of Art Basement" (wrong)
+    "MC": "Marriott Center",                      # was: "Monte L. Bean Museum" (wrong)
+    "MCA": "Marriott Center Annex",
+    "MCDB": "McDonald Building",
+    "MCKB": "David O. McKay Building",
+    "MLBM": "Monte L. Bean Life Science Museum",
     "MLRS": "MLRS",
     "MOA": "Museum of Art",
-    "MSRB": "MSRB",
-    "NICB": "Nicholes Building",
+    "MSRB": "Karl G. Maeser Building",
+    "NICB": "Joseph K. Nicholes Building",
     "NUF": "NUF",
-    "RB": "Richards Building",
+    "RB": "Stephen L. Richards Building",
     "RBF": "Richards Building Fieldhouse",
-    "ROTC": "ROTC Building",
-    "SAB": "Smoot Administration Building",
-    "SFH": "SFH",
+    "ROTC": "Daniel H. Wells Building",           # was: "ROTC Building"
+    "SAB": "Student Athlete Building",            # was: "Smoot Administration Building" (wrong)
+    "SFH": "George Albert Smith Fieldhouse",
     "SFLD": "Soccer Field",
     "SLC": "Salt Lake Center",
-    "SNLB": "Snell Building",
-    "TCB": "Thomas L. Clyde Building",
+    "SNLB": "William H. Snell Building",
+    "TCB": "Tennis Courts Building",              # was: "Thomas L. Clyde Building" (wrong)
     "TCF": "TCF",
-    "TLRA": "Towers",
-    "TLRB": "Towers B",
-    "TMCB": "Talmage Building",
-    "TNRB": "Tanner Building",
+    "TLRA": "Taylor Building Annex",              # was: "Towers" (wrong)
+    "TLRB": "John Taylor Building",               # was: "Towers B" (wrong)
+    "TMCB": "James E. Talmage Building",
+    "TNRB": "N. Eldon Tanner Building",
     "TRAK": "Track & Field",
     "TRPN": "TRPN",
     "TRPS": "TRPS",
-    "UPC": "University Press / Creative Works",
+    "UPC": "University Parkway Center",           # was: "University Press / Creative Works" (wrong)
     "WCAS": "WCAS",
     "WCCB": "WCCB",
     "WCCL": "WCCL",
     "WCOB": "Marriott School of Business",
     "WSC": "Wilkinson Student Center",
-    "WVB": "Wymount Village",
+    "WVB": "West View Building",                  # was: "Wymount Village" (wrong)
 }
 
 ALL_BUILDINGS: list[str] = [
